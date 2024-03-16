1b10b8868baef7dbe40c9d4b80fbd3dde2f23554
Arkadiusz Michowski <arkadiusz@michowski.com>
2024-03-16 23:23:25 +0100

    it works!!

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
  rm -rf output && mapred streaming \
 -files mapper.py,reducer.py \
--input input/station.csv -output output \
+-input input/datasource1 -output output \
 -mapper mapper.py -reducer reducer.py
